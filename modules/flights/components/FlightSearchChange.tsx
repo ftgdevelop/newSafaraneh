cb8e90b6a5567e373691a6421ab228587738dd1a
Ali Adibpour <aliadp0101@gmail.com>
2024-04-24 14:53:35 +0330

    improve style flight Item in mobile

--- modules/flights/components/FlightSearchChange.tsx
+++ modules/flights/components/FlightSearchChange.tsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setSearchChangeOn } from "../store/flightsSlice";
 import FlightSearch from "./flightSearch/FlightSearch";
 
-const FlightsSearchChange: React.FC = () => {
+const FlightsSearchChange: React.FC<any> = ({airports}) => {
     const SearchChangeOn = useSelector((state: RootState) => state.flightFilters.SearchChangeOn)
     const dispatch = useDispatch()
     return (
@@ -17,7 +17,7 @@ const FlightsSearchChange: React.FC = () => {
                             بستن
                         </button>
                 </div>
-                <FlightSearch className="pl-4 pr-4 max-sm:p-0" />
+                <FlightSearch className="pl-4 pr-4 max-sm:p-0" airports={airports} />
         </div>
 
         <div className={`z-20 fixed top-0 left-0 bg-black/30 backdrop-blur-sm ${!SearchChangeOn ? 'hidden' : 'w-full h-full'}`}
