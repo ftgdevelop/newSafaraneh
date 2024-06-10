3c2bc7c3ba8f7430acec698a3c1873d2c65dde4d
s.feli <samran.f.m@gmail.com>
2024-06-10 09:35:52 +0330

    set modules for 1stsafar

--- next.config.js
+++ next.config.js
@@ -69,8 +69,8 @@ const nextConfig = {
     PROJECT_SERVER_TENANTID: "1040",
     PROJECT_SERVER_NATIONALITY: "",
     PORT: '',
-    //PROJECT_MODULES: "Flight Hotel CIP Blog ForeignFlight ForeignHotel",
-    PROJECT_MODULES: "DomesticHotel",
+    //PROJECT_MODULES: "DomesticHotel DomesticFlight CIP Blog ForeignFlight ForeignHotel",
+    PROJECT_MODULES: "DomesticHotel DomesticFlight CIP Blog",
     // DEFAULT_lAN:"US",
     // LANGUAGES:["US","NO","FA"]
 
