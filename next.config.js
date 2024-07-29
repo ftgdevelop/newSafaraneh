2dd6aa882729d57842a6411f6643c10bc61f38f0
s.feli <samran.f.m@gmail.com>
2024-07-29 19:20:34 +0330

    set mrbilet blog endpoint

--- next.config.js
+++ next.config.js
@@ -58,7 +58,7 @@ const nextConfig = {
     PROJECT_SERVER_HOTEL_DATA: "hoteldomesticdata.safaraneh.com",
     PROJECT_SERVER_HOTEL_AVAILABILITY: "hotelv5.safaraneh.com",
     PROJECT_SERVER_COORDINATOR:"coordinator.safaraneh.com",
-    PROJECT_SERVER_BLOG :"blogonline.ir/iranhotel",
+    PROJECT_SERVER_BLOG :"blogonline.ir/mrbilet",
     PROJECT_SERVER_CRM:"crm.safaraneh.com",
     PROJECT_SERVER_PAYMENT: "payline.samita.com",
     PROJECT_SERVER_IDENTITY:"identity.safaraneh.com",
