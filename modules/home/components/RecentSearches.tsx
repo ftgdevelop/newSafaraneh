4b7bca840900279930ca46b1d247da66da855fa1
s.feli <samran.f.m@gmail.com>
2024-09-18 15:07:43 +0330

    improve recent search grid style

--- modules/home/components/RecentSearches.tsx
+++ modules/home/components/RecentSearches.tsx
@@ -40,7 +40,7 @@ const RecentSearches : React.FC<Props>= props => {
                 </button>
             </div>
 
-            <div className={`grid grid-cols-2 sm:grid-cols-4 gap-3 pb-3 ${theme2?"":"lg:grid-cols-5"}`}>
+            <div className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3 pb-3 ${theme2?"":"lg:grid-cols-5"}`}>
                 {slicedItems.map((item, index) => <RecentSearchItem
                     key={index}
                     model={{
