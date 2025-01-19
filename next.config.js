50b27f3e9e69a888ebd3ab9a44270ae1620e975c
s.feli <samran.f.m@gmail.com>
2025-01-19 13:17:31 +0330

    disable blog

--- next.config.js
+++ next.config.js
@@ -64,7 +64,7 @@ const nextConfig = {
     PROJECT_SERVER_HOTEL_DATA: "hoteldomesticdata.safaraneh.com",
     PROJECT_SERVER_HOTEL_AVAILABILITY: "hotelv5.safaraneh.com",
     PROJECT_SERVER_COORDINATOR:"coordinator.safaraneh.com",
-    PROJECT_SERVER_BLOG :"blogonline.ir/iranhotel",
+    //PROJECT_SERVER_BLOG :"blogonline.ir/iranhotel",
     PROJECT_SERVER_CRM:"crm.safaraneh.com",
     PROJECT_SERVER_PAYMENT: "payline.samita.com",
     PROJECT_SERVER_IDENTITY:"identity.safaraneh.com",
@@ -76,7 +76,7 @@ const nextConfig = {
     PROJECT_SERVER_NATIONALITY: "",
     PORT: '',
     //PROJECT_MODULES: "Flight Hotel CIP Blog ForeignFlight ForeignHotel",
-    PROJECT_MODULES: "DomesticFlight DomesticHotel CIP Blog",
+    PROJECT_MODULES: "DomesticFlight DomesticHotel CIP",
     // DEFAULT_lAN:"US",
     // LANGUAGES:["US","NO","FA"]
 
