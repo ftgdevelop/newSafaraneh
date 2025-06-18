2172a783c257881be3720bc59eb13a63db518e6f
Samran <samran.f.m@gmail.com>
2025-06-18 12:38:00 +0330

    fix conflict

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -38,35 +38,7 @@ function SiteMap() {}
 
 export const getServerSideProps = async ({ res }:{res:any}) => {
 
-<<<<<<< Updated upstream
-
-  const [postsResponse, pagesResponse, flightResponse] = await Promise.all([
-    
-    axios.get(`${process.env.PROJECT_SERVER_TYPE}${process.env.PROJECT_SERVER_BLOG}//wp-json/wp/v2/posts?per_page=100`).catch(function (error) {
-      console.log("error");
-    })
-    ,
-    axios.get(
-      "https://cms2.safaraneh.com/api/services/app/Page/GetAll",
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept-Language': 'fa-IR',
-          apikey: process.env.PROJECT_PORTAL_APIKEY,
-        },
-      }).catch(function (error) {
-        console.log("error");
-      })    
-    ,
-    axios.get(`${process.env.PROJECT_SERVER_BLOG}wp-json/wp/v2/flightdomestic`).catch(function (error) {
-      console.log("error");
-    })
-  ]);
-
-  const sitemap = creareSiteMap(pagesResponse?.data?.result , postsResponse?.data, flightResponse?.data );
-=======
   const sitemap = creareSiteMap();
->>>>>>> Stashed changes
 
   res.setHeader('Content-Type', 'text/xml');
   res.write(sitemap);
