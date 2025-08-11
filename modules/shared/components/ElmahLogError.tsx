f16d225cc94095cfc149d155e915574ab1cec4c2
s.feli <samran.f.m@gmail.com>
2025-08-11 21:40:39 +0330

    add elmah.io

---
+++ modules/shared/components/ElmahLogError.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react"
+import Elmahio from "elmah.io.javascript";
+
+const ElmahLogError = () => {
+
+    useEffect(()=>{
+        new Elmahio({
+          apiKey: '8ae913b3ecf94e3284d4e8c45296b216',
+          logId: 'fcbc4389-0026-44a1-a1eb-63973a61002a'
+        });
+    },[]);
+
+    return(
+        null
+    )
+}
+
+export default ElmahLogError;
