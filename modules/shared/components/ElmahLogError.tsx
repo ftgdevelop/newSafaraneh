86ad4bc4bd34f9c8b9968d755c676ad1f5540916
s.feli <samran.f.m@gmail.com>
2025-08-12 15:51:04 +0330

    add elmah.io

--- modules/shared/components/ElmahLogError.tsx
+++ modules/shared/components/ElmahLogError.tsx
@@ -4,13 +4,11 @@ import Elmahio from "elmah.io.javascript";
 const ElmahLogError = () => {
 
     useEffect(() => {
-        const logger = new Elmahio({
+        new Elmahio({
             apiKey: '8ae913b3ecf94e3284d4e8c45296b216',
             logId: 'fcbc4389-0026-44a1-a1eb-63973a61002a',
             application: 'hotelBan js'
         });
-
-        logger.verbose('تستتت');
     }, []);
 
     return (
