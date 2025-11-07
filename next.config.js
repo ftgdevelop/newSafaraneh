692f01ed50bea5e6f368d505c280b917fda81369
s.feli <samran.f.m@gmail.com>
2025-11-07 16:37:28 +0330

    set torbeh tenantId and apiKey

--- next.config.js
+++ next.config.js
@@ -58,7 +58,7 @@ const nextConfig = {
     GOOGLE_ANALYTIC_ID: 'G-L9XG7Q3D68',
     GOOGLE_TAG_MANAGER_ID: 'G-1EXTLDBKNV',
 
-    PROJECT_SERVER_APIKEY: '602d3109-6e40-4653-42a4-08dbc10a4f4a',
+    PROJECT_SERVER_APIKEY: '73de94ec-f921-4775-b337-d18039baad04',
     PROJECT_PORTAL_APIKEY: '92ba7928-ab01-4011-be49-5bed7556ae2a',
     
     PROJECT_SERVER_CMS:"cms.hotelban.com",
@@ -75,7 +75,7 @@ const nextConfig = {
     PROJECT_SERVER_CIP:"cip.safaraneh.com",
     PROJECT_SERVER_FLIGHT: "flightdomestic.hotelban.com",
 
-    PROJECT_SERVER_TENANTID: "1047",
+    PROJECT_SERVER_TENANTID: "1052",
 
     PROJECT_SERVER_STRAPI:"strapi.safaraneh.com",
     PROJECT_SERVER_STRAPI_TOKEN : "de7c6a2c34c477963c83c51ad5b6b6be760002e52d53ba39ca05c20a68c8de23393c65ed1eab854d5f6e73af5e876bb4770ac7c402f92775b9dc6cffe464f98d7bf8e8a51e214d01632871b4e091ef309f986446893634e70f9a71602fa0a060c395084de4b4b9cde3b63d09738319235129170f1805a940a62716df7829d047",
@@ -84,7 +84,7 @@ const nextConfig = {
     PROJECT_SERVER_NATIONALITY: "",
     PORT: '',
     //PROJECT_MODULES: "DomesticHotel DomesticFlight CIP Blog ForeignFlight ForeignHotel",
-    PROJECT_MODULES: "DomesticHotel",
+    PROJECT_MODULES: "DomesticHotel DomesticFlight",
     // DEFAULT_lAN:"US",
     // LANGUAGES:["US","NO","FA"]
 
