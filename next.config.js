fb8276651607c75e8fb89534ffc80facc7b55844
s.feli <samran.f.m@gmail.com>
2025-12-16 11:55:23 +0330

    disable domestic flight

--- next.config.js
+++ next.config.js
@@ -84,7 +84,7 @@ const nextConfig = {
     PROJECT_SERVER_NATIONALITY: "",
     PORT: '',
     //PROJECT_MODULES: "Flight Hotel CIP Blog ForeignFlight ForeignHotel",
-    PROJECT_MODULES: "DomesticFlight DomesticHotel CIP",
+    PROJECT_MODULES: "DomesticHotel CIP",
     // DEFAULT_lAN:"US",
     // LANGUAGES:["US","NO","FA"]
 
